3ab8f15971e7abde348744ff786427d1bc06b49f
Joey Ortiz <joeyortiz2016@gmail.com>
2025-04-17 19:15:20 -0700

    Initialized Spring Web project

---
+++ src/main/java/com/cleanmusic/CleanmusicApplication.java
@@ -0,0 +1,13 @@
+package com.cleanmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleanmusic/CleanmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
